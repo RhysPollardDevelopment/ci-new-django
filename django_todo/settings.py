479c28978414545f47cc89bc9169bd5ce22bb136
Rhys Pollard <rpollard12@hotmail.co.uk>
2021-02-25 11:50:09 +0000

    Updated allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 's7@0hwjfot=5cy!6tdvmh#+0kfbdzapx3coj5e#9$)(f@@(vl8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rpollard-django-todo-app.herokuapp.com']
 
 
 # Application definition
